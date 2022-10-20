4df4dd189d83663132e960369e5f6a252360000f
mimiyama <lmizuyama@gmail.com>
2022-10-20 12:28:16 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ $('input:radio[name="opcao"]').change(function() {
     $(this).attr("checked", true);
   }
 });
+
+function salvarPessoa(){
+  var tipoD = document.getElementById("tipoDef").value;
+  var celularD = document.getElementById("celDef").value;
+
+  // Pega a lista já cadastrada, se não houver vira um array vazio
+  var lista_pessoas = JSON.parse(localStorage.getItem('lista-pessoas') || '[]');
+  // Adiciona pessoa ao cadastro
+  lista_pessoas.push({
+      tipoD: tipoD,
+      celularD: celularD
+  });
+
+  // Salva a lista alterada
+  localStorage.setItem("lista-pessoas", JSON.stringify(lista_pessoas));
+
+  console.log('Salva com sucesso.');
+}
